757d144af0bdb22f0b0f367ef8aa8cb56bbb48f8
sample777 <sample777@noreply.bitbucket.org>
2025-12-08 14:20:29 +0000

    Add API client implementation

---
+++ api_client.py
@@ -0,0 +1,9 @@
+# API Client
+import requests
+
+class APIClient:
+    def __init__(self, base_url):
+        self.base_url = base_url
+    
+    def get(self, endpoint):
+        return requests.get(f'{self.base_url}/{endpoint}')
